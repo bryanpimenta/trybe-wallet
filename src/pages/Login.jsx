12b0e96c9bd1c104c9696872694033d71b88ab6f
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-05-12 17:03:37 -0300

    Requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addEmail } from '../redux/actions';
+
+class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isDisable: true,
+  };
+
+  onInputChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.checkEmail);
+  };
+
+  checkEmail = () => {
+    const { email, password } = this.state;
+    const defaultEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const minLength = 6;
+    if (password.length >= minLength && defaultEmail.test(email)) {
+      return this.setState({ isDisable: false });
+    }
+    return this.setState({ isDisable: true });
+  };
+
+  submitLogin = () => {
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(addEmail(email));
+    history.push('/carteira');
+  };
+
+  render() {
+    const { isDisable } = this.state;
+
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          name="email"
+          type="email"
+          onChange={ this.onInputChange }
+        />
+
+        <input
+          data-testid="password-input"
+          name="password"
+          type="password"
+          onChange={ this.onInputChange }
+        />
+        <button onClick={ this.submitLogin } disabled={ isDisable }>Entrar</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.email,
+});
+
+export default connect(mapStateToProps)(Login);
+
+Login.propTypes = {
+  dispatch: PropTypes.func,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const loginReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ADD_EMAIL:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default loginReducer;
