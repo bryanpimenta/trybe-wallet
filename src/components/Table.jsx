2706c7c4ac125f666238ba49c277dcef04adb79b
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-05-13 18:06:17 -0300

    Requisito 6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 
 class Table extends Component {
   render() {
+    /* const { expenses } = this.props; */
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </table>
     );
   }
 }
+
+/* const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+}); */
 
 export default Table;
